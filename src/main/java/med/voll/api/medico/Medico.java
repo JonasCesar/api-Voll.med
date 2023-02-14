8f000265e2514b7df1bafac323490bfa2d8b702c
Jonas César Silva <jonascesar.silva@dxc.com>
2023-02-14 18:14:20 -0300

    Crud de médico de aplicação completo

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -8,33 +8,28 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
-@Entity(name = "medicos")
-@Table(name = "Medico")
+@Entity(name = "Medico")
+@Table(name = "medicos")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Medico {
 	
-	public Medico(DadosCadastroMedico dados) {
-		this.nome = dados.nome();
-		this.email = dados.email();
-		this.crm = dados.crm();
-		this.especialidade = dados.especialidade();
-		this.endereco = new Endereco(dados.endereco());
-	}
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
+	private String telefone;
 	private String crm;
 	
 	@Enumerated(EnumType.STRING)
@@ -42,5 +37,35 @@ public class Medico {
 	
 	@Embedded
 	private Endereco endereco;
+	
+	private Boolean ativo;
+
+	public Medico(DadosCadastroMedico dados) {
+		this.ativo = true;
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.crm = dados.crm();
+		this.especialidade = dados.especialidade();
+		this.endereco = new Endereco(dados.endereco());
+	}
+	
+	public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+		if(dados.nome() != null) {
+			this.nome = dados.nome();
+		}
+		if(dados.telefone() != null) {
+			this.telefone = dados.telefone();
+		}
+		if(dados.endereco() != null) {
+			this.endereco.atualizarInformacoes(dados.endereco());
+		}
+	}
+
+	public void excluir() {
+		this.ativo = false;
+	}
+
+	
 
 }
