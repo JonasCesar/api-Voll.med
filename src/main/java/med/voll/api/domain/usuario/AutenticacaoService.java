0ff36bcb3b9d9839065a7f672634fb622a167366
Jonas César Silva <jonascesar.silva@dxc.com>
2023-02-21 16:46:53 -0300

    classes UsuarioRepository e AutenticacaoService

---
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+/* o sprint automaticamente encontra essa classe e chama no momento da autenticação
+ desde que tenha a anotação service e implemente o UserDetailsService */
+@Service
+public class AutenticacaoService implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return repository.findByLogin(username);
+	}
+
+}
