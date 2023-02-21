0f6835e90b391a9aaf339fd574cdf02914409e04
Jonas César Silva <jonascesar.silva@dxc.com>
2023-02-21 10:47:46 -0300

    Implementando o detalhar médico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -68,5 +68,11 @@ public class MedicoController {
 		//irá retornar o código 204 (boa prática para retornos de exclusão)
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity detalhar(@PathVariable Long id) {
+		var medico = repository.getReferenceById(id);
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+	}
 
 }
