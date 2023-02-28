67675d1a386e2516c8bc8dced692f01273698c87
Jonas César Silva <jonascesar.silva@dxc.com>
2023-02-28 20:20:17 -0300

    Indicando para o spring que nosso filtro vem antes do filtro do Spring

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -20,12 +20,13 @@ public class SecurityFilter extends OncePerRequestFilter{
 	@Autowired
 	private TokenService tokenService;
 	
+	@Autowired
 	private UsuarioRepository usuarioRepository;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
+
 		var tokenJWT = recuperarToken(request);		
 		
 		if(tokenJWT != null) {
