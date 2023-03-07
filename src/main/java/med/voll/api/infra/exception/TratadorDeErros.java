a45927b7825c1118179cc7ebdba1f008180d163c
Jonas César Silva <jonascesar.silva@dxc.com>
2023-03-07 19:43:08 -0300

    Cadastrar paciente

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,7 +14,7 @@ public class TratadorDeErros {
 	
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity tratarErro404() {
-		return ResponseEntity.notFound().build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Página não encontrada");
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
